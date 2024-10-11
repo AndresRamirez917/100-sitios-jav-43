e3abc011f97b5387c84a14c808f774bcdbc4e434
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-11 16:34:34 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -2,29 +2,26 @@ async function getData(){
     const result = await fetch('https://fakestoreapi.com/products/');
     const products = await result.json();
     const productsArr = products.map(elemento => Object.entries(elemento));
-    const productsArrSliced = productsArr.slice(0,4);
+    //id productos específicos a buscar
+    const arr = [4,5,6]
     console.log(products);
     products.forEach(element => {      
-        const randInt = randomData(1, productsArr.length);
-        const randIndex = randInt;
-        for(i = 0; i < productsArrSliced.length; i++){
-            if(element.id == i){
-                const box = document.createRange().createContextualFragment(`
-                    
-                    
-                <div class="box box-1">
-                    <h3>${productsArr[randIndex][1][1]}</h3>
-                    <img src="${productsArr[randIndex][5][1]}" alt="">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque dolores, et officiis omnis officia laudantium eos?</p>
-                </div>
-                    
-                    `)
-                    const testimonials_row = document.querySelector('.testimonials-row');
-                    testimonials_row.append(box)
+        for(i = 0; i < 2; i++){
+            for(j = 0; j < arr.length; j++){
+                if(element.id == i){
+                    const box = document.createRange().createContextualFragment(`
+                                          
+                    <div class="box box-1">
+                        <h3>${productsArr[arr[j]][1][1]}</h3>
+                        <img src="${productsArr[arr[j]][5][1]}" alt="">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque dolores, et officiis omnis officia laudantium eos?</p>
+                    </div>
+                        
+                        `)
+                        const testimonials_row = document.querySelector('.testimonials-row');
+                        testimonials_row.append(box)
+                }
             }
-        }
-        function randomData(min, max){
-            return Math.floor(Math.random() * (min - max + 1) + max);
         }
     });
 }
