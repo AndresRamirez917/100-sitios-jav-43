418ecb4fa26c83342e3a44b4d85d3c891317de6a
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-11 16:57:06 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -5,7 +5,8 @@ async function getData(){
     //id productos específicos a buscar
     const arr = [4,5,6]
     console.log(products);
-    products.forEach(element => {      
+    products.forEach(element => {    
+        //este bucle no tengo idea como funciona  
         for(i = 0; i < 2; i++){
             for(j = 0; j < arr.length; j++){
                 if(element.id == i){
@@ -25,5 +26,46 @@ async function getData(){
         }
     });
 }
+
+const btn_validar = document.getElementById('btn-validar');
+const validar = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje');
+    const arr = [];
+    const messageArr = ["Nombre", "Email", "Mensaje"];
+    arr.push(nombre, email, mensaje);
+    for(i = 0; i < arr.length; i++){
+        if(arr[i].value == ""){
+            swal({
+                title: `El campo ${messageArr[i]} no puede estar vacío`,
+                icon: "error",
+                 })
+                 return false;
+        }
+    }
+    if(!emailValido(email)){
+        swal({
+            title: `El campo ${messageArr[1]} no tiene el formato adecuado`,
+            icon: "error",
+             })
+             return false;
+    }
+    swal({
+        title: `Datos enviados satisfactoriamente`,
+        icon: "success",
+         })
+         nombre.value = "";
+         email.value = "";
+         mensaje.value = "";
+    return true;
+}
+
+const emailValido = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)
+}
+
+btn_validar.addEventListener("click", validar)
 
 getData()
